176ab58c479d2b26115726930d26a03689705bde
Simon Regenwetter <64084644+simrgt@users.noreply.github.com>
2022-11-22 14:15:10 +0100

    étape 1

---
+++ src/sources/hex/Plateau.java
@@ -0,0 +1,49 @@
+package sources.hex;
+
+public class Plateau {
+	private final static int TAILLE_MAX = 26;
+	private final static int NB_JOUEURS = 2;
+	
+	private Pion[][] t;
+
+	public Plateau(int taille) {
+		assert taille > 0 && taille <= TAILLE_MAX;
+		
+		t = new Pion [taille][taille];
+		
+		for (int lig = 0; lig < taille(); ++lig)
+			for (int col = 0; col < taille(); ++col)
+				t[col][lig] = Pion.Vide;
+	}
+	
+	public int taille() {
+		return t.length;
+	}
+	
+	
+	private String espaces(int n) {
+		String s = "";
+		for(int i = 0; i < n; ++i)
+			s+= " ";
+		return s;
+	}
+	@Override
+	public String toString() {
+		String s = "";
+		for (int i = 0; i < taille(); ++i)
+			s+=" "+(char)( 'A' + i);
+		s+='\n';
+		for (int lig = 0; lig < taille(); ++lig) {
+			s+= lig+1 + espaces (lig);
+			for (int col = 0; col < taille(); ++col)
+				s+= " "+ t[col][lig];
+			s+='\n';
+		}
+		return s;
+	}
+
+
+
+
+
+}
---
+++ src/tests/hex/PlateauTest.java
@@ -0,0 +1,32 @@
+package tests.hex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import sources.hex.Pion;
+import sources.hex.Plateau;
+
+class PlateauTest {
+
+	@Test
+	void test() {
+		final int taille = 4;
+		Plateau p = new Plateau(taille);
+		assertEquals(taille, p.taille());
+		
+		System.out.println(p);
+		assertEquals(
+				" A B C D\n" + 
+				"1 . . . .\n" + 
+				"2  . . . .\n" + 
+				"3   . . . .\n" + 
+				"4    . . . .\n", p.toString());
+		
+	
+		// jouer un coup en B2
+		//assertEquals(Pion.Croix, p.getCase("B2"));
+		
+	}
+
+}
